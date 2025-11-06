a6c9ae319d361fd77dc46de6a84a7f8cfe6a4a6d
TheEinfachFelix <The.Einfach.Felix@gmail.com>
2025-11-06 20:47:38 +0000

    running jea

--- Moduls/Ausguck/frontend/ausguck/vite.config.ts
+++ Moduls/Ausguck/frontend/ausguck/vite.config.ts
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+    build: {
+      outDir: 'build'  // falls du wirklich build/ willst
+    },
     server: {
-    host: true
+    host: '0.0.0.0',
+    port: 4173,
+    //allowedHosts: ['einfachfelix.ydns.eu']  // Erlaubt den Zugriff über deine Domain
+
   }
+  
 })
