5ebb28f4a16bd0450997fd1ed12d99f4c4a11708
TheEinfachFelix <The.Einfach.Felix@gmail.com>
2025-11-06 21:14:19 +0000

    api con not working

--- Moduls/Ausguck/frontend/ausguck/src/hooks/useApi.ts
+++ Moduls/Ausguck/frontend/ausguck/src/hooks/useApi.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL as string;
 
 function SubmitSurevey(surveyData: any) : Promise<boolean> {
-    return axios.post(apiUrl, surveyData).then(() => {
+    return axios.post("https://einfachfelix.ydns.eu/api/registration", surveyData).then(() => {
         return true;
     }).catch(() => {
         return false;
--- Moduls/AusguckBackend/AusguckBackend/Program.cs
+++ Moduls/AusguckBackend/AusguckBackend/Program.cs
@@ -21,7 +21,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseExceptionHandler("/error");
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
