64361bdd9d0b6181776a84e8d219a5c80a53e34e
TheEinfachFelix <The.Einfach.Felix@gmail.com>
2025-11-07 14:12:53 +0000

    mm

--- Moduls/AusguckBackend/AusguckBackend/Program.cs
+++ Moduls/AusguckBackend/AusguckBackend/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddScoped<IRegistrationService, RegistrationService()>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
