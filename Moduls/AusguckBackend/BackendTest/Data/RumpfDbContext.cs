facf29e5c432bfc0539aabb008636814c7fa6784
TheEinfachFelix <The.Einfach.Felix@gmail.com>
2025-12-19 12:32:21 +0100

    small Fix

--- Moduls/AusguckBackend/BackendTest/Data/RumpfDbContext.cs
+++ Moduls/AusguckBackend/BackendTest/Data/RumpfDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using AusguckBackend;
 using BackendTest.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -45,8 +46,7 @@ public partial class RumpfDbContext : DbContext
     public virtual DbSet<tag> tags { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=192.168.178.143;Database=Rumpf;Username=AusguckTester;Password=dsafadsfef25ojnkoajn9oiujbasounsaejlkwsxx");
+        => optionsBuilder.UseNpgsql(Globals.TestConnectionString);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
