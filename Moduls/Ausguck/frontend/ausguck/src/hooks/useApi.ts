e9c0461dbba7463314b1a038af7fde4714055507
TheEinfachFelix <The.Einfach.Felix@gmail.com>
2025-12-19 11:26:24 +0000

    Done Working Tested

--- Moduls/Ausguck/frontend/ausguck/src/hooks/useApi.ts
+++ Moduls/Ausguck/frontend/ausguck/src/hooks/useApi.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+import ApiUrl from "./ApiUrl";
 
 function SubmitSurevey(surveyData: any) : Promise<boolean> {
-    return axios.post("https://einfachfelix.ydns.eu/api/registration", surveyData).then(() => {
+    return axios.post(ApiUrl + "/registration", surveyData).then(() => {
         return true;
     }).catch(() => {
         return false;
